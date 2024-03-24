afee47dedd55c6ca435744c82a375c8fc0956149
Rafael020202 <luizrafael391@gmail.com>
2024-03-24 11:47:17 -0300

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListScheduleComponent } from './components/schedule/list-schedule/list-schedule.component';
+import { AddToScheduleComponent } from './components/schedule/add-to-schedule/add-to-schedule.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'listSchedule',
+    pathMatch: 'full'
+  },
+  {
+    path: 'listSchedule',
+    component: ListScheduleComponent
+  },
+  {
+    path: 'addToSchedule',
+    component: AddToScheduleComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListScheduleComponent } from './components/schedule/list-schedule/list-schedule.component';
+import { HeaderComponent } from './components/header/header.component';
+import { AddToScheduleComponent } from './components/schedule/add-to-schedule/add-to-schedule.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListScheduleComponent,
+    HeaderComponent,
+    AddToScheduleComponent
   ],
   imports: [
+    BrowserAnimationsModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatButtonModule,
+    MatIconModule,
+    MatTooltipModule
   ],
   providers: [],
   bootstrap: [AppComponent]
